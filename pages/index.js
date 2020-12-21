56d78ad981b6a89915dfc4bfba291c3b3b013562
Frodo Baggins <frodo23@protonmail.com>
2020-12-21 22:26:34 +0100

    netflify envs debug

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,6 @@ import RecordData from '../components/record-data'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+	console.log('test:', process.env.TEST)
 	return <RecordData />
 }
