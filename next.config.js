098b91991e811914e9b9d972d4e1465ca90860d7
Frodo Baggins <frodo23@protonmail.com>
2020-12-21 22:59:42 +0100

    netflify api debug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    env:{
-        TEST: process.env.TEST,
-        GS_CLIENT_EMAIL:process.env.GS_CLIENT_EMAIL,
-        GS_PRIVATE_KEY:process.env.GS_PRIVATE_KEY
-    }
+	target: 'serverless',
+	env: {
+		TEST: process.env.TEST,
+		GS_CLIENT_EMAIL: process.env.GS_CLIENT_EMAIL,
+		GS_PRIVATE_KEY: process.env.GS_PRIVATE_KEY,
+	},
 }
